95e032a0fd3f395171abe9c18ad03e6e83e61738
Shakhnoza <qadamboevashaxnoza@gmail.com>
2025-12-06 15:25:48 +0500

    ...

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,1 +1,16 @@
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv"
+config()
+const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true})
+
+bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+    const firstname = msg.chat.first_name;
+    const text = msg.text;
+    bot.sendMessage(chatId, `Assalomu Aleykum ${firstname}`)
+    bot.sendMessage(chatId, `${text}`)
+})
+
+
+
 console.log("BOT ISHGA TUSHDI...");
