bab68146672ce285ddccf0f4d71607c677bb0596
Shakhnoza <qadamboevashaxnoza@gmail.com>
2025-12-06 15:52:14 +0500

    ............

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,16 +1,27 @@
 import TelegramBot from "node-telegram-bot-api";
-import { config } from "dotenv"
-config()
-const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true})
+import { config } from "dotenv";
+import onStart from "./handlers/onStart.js";
+import onProfile from "./handlers/onProfile.js";
+import onError from "./handlers/onError.js";
+config();
+
+export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.on("message", (msg) => {
-    const chatId = msg.chat.id;
-    const firstname = msg.chat.first_name;
-    const text = msg.text;
-    bot.sendMessage(chatId, `Assalomu Aleykum ${firstname}`)
-    bot.sendMessage(chatId, `${text}`)
-})
+  const chatId = msg.chat.id;
+  const firstname = msg.chat.first_name;
+  const text = msg.text;
 
+  if (text == "/start") {
+    return onStart(msg);
+  }
 
+  if (text == "/profile") {
+    return onProfile(msg);
+  }
 
-console.log("BOT ISHGA TUSHDI...");
+  return onError(msg);
+});
+
+console.log("Bot ishga tushdi...");
+
---
+++ src/bot/handers/onProfile.js
@@ -0,0 +1,20 @@
+import { bot } from "../bot.js";
+
+function onProfile(msg) {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(
+    chatId,
+    `
+  SHAXSIY PROFIL:
+
+- CHAT-ID: ???
+- ISMI: ????
+- USERNAME: ????
+- ACTIVE: ???
+- BALANCE: ???  
+`
+  );
+}
+
+export default onProfile;
