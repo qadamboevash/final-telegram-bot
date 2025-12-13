41bc335b53cd9b3e18b0e62654e13f2406eaca2a
Shakhnoza <qadamboevashaxnoza@gmail.com>
2025-12-13 15:49:01 +0500

    done

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -7,10 +7,57 @@ config();
 
 export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-bot.on("message", (msg) => {
+const CHANNEL_ID = "@academy_100x_uz";
+
+const checkIfUserSubscribed = async (chatId) => {
+  try {
+    const chatMember = await bot.getChatMember(CHANNEL_ID, chatId);
+    console.log(chatMember.status);
+
+    if (chatMember.status == "left" || chatMember.status == "kicked") {
+      return false;
+    } else {
+      return true;
+    }
+  } catch {
+    console.log("error: chatMember checking");
+  }
+};
+
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const firstname = msg.chat.first_name;
   const text = msg.text;
+
+
+  const user_subscribed = await checkIfUserSubscribed(chatId);
+
+  console.log(user_subscribed);
+
+  if (user_subscribed == false) {
+    return bot.sendMessage(
+      chatId,
+      `Hurmatli ${firstname}, \nSiz botimizdan foydalanishingiz uchun oldin quyidagi kanalga obuna bo'lishing garak... 👇`,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: `100x Academy Xiva`,
+                url: "https://t.me/academy_100x_uz",
+              },
+            ],
+            [
+              {
+                text: `Obunani tekshirish `,
+                callback_data: "confirm_subscribtion",
+              },
+            ],
+          ],
+        },
+      }
+    );
+  }
 
   if (text == "/start") {
     return onStart(msg);
@@ -21,6 +68,29 @@ bot.on("message", (msg) => {
   }
 
   return onError(msg);
+});
+
+bot.on("callback_query", async (query) => {
+  const msg = query.message;
+  const data = query.data;
+  const queryId = query.id;
+
+  const chatId = msg.chat.id;
+  const firstname = msg.chat.first_name;
+
+  if (data == "confirm_subscribtion") {
+    console.log("TUGMA BOSILDIII");
+    const user_subscribed = await checkIfUserSubscribed(chatId);
+
+    if (user_subscribed == false) {
+      return bot.answerCallbackQuery(queryId, {
+        text: "Siz hali obuna bo'lmadingiz... ❌",
+      });
+    } else {
+      bot.deleteMessage(chatId, msg.message_id);
+      return onStart(msg);
+    }
+  }
 });
 
 console.log("Bot ishga tushdi...");
