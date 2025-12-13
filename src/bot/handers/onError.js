fb27ac50cb108c8c3e8c47fa47c95a81effe2112
Shakhnoza <qadamboevashaxnoza@gmail.com>
2025-12-13 14:38:46 +0500

    ....

--- src/bot/handers/onError.js
+++ src/bot/handers/onError.js
@@ -3,7 +3,7 @@ import { bot } from "../bot.js";
 function onError(msg) {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, `Kutilmagan xatolik... /start bosing! `);
+  bot.sendMessage(chatId, `Kutilmagan xatolik...❗️ /start bosing! `);
 }
 
 export default onError;
